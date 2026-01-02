2b1428dbd61048e2821a1fcd0b2d62af3ddedd7a
Furkan <furkanakin1903@gmail.com>
2026-01-02 17:21:52 +0300

    Sadece_Guncelle.bat daha bilgilendirici hale getirildi.

--- Sadece_Guncelle.bat
+++ Sadece_Guncelle.bat
@@ -3,6 +3,10 @@ title Telegram Macro Sadece Guncelle
 echo [1/2] Guncellemeler kontrol ediliyor...
 git fetch origin main
 git reset --hard origin/main
+echo.
+echo Mevcut Versiyon:
+git log -1 --pretty=format:"%%h - %%s (%%cr)"
+echo.
 echo [2/2] NPM paketleri guncelleniyor...
 call npm install
 echo.
